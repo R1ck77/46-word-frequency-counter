8f168dedec86ac18229268c56f2b6863239b4128
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-29 00:16:12 +0200

    Add table manipulation functions

--- find-words-frequency.el
+++ find-words-frequency.el
@@ -3,14 +3,14 @@
 (defun find-words-frequency ()
   "Print the frequency of each word in the current region in a separate buffer"
   (interactive)
-  (message (format "%s" (fwf-all-words (region-beginning) (region-end)))))
+  (message (format "%s" (fwf--all-words (region-beginning) (region-end)))))
 
-(defun update-word (word hash-table)
+(defun fwf--update-word (word hash-table)
   (puthash word
            (+ (gethash word hash-table 0) 1)
            hash-table))
 
-(defun fwf-all-words (from to)
+(defun fwf--all-words (from to)
   "Return a list of all words in the range specified
 
 Some edge cases are not handled properly, but it's outside the scope of the exercise
@@ -30,4 +30,30 @@ the word mushroom is gonna be counted."
         (setq results (cons (thing-at-point 'word) results))))
     results))
 
+(defun fwf--count-words (from to)
+  (lexical-let ((table (make-hash-table :test 'equal)))
+    (mapc (lambda (x)
+            (fwf--update-word x table))
+          (fwf--all-words))))
 
+(defun fwf--sorted-values (table)
+  (sort
+   (fwf--unsorted-values table)
+   (lambda (a b)
+     (> (car a) (car b)))))
+
+(defun fwf--unsorted-values (table)
+  (let ((acc '()))
+    (maphash (lambda (key value)
+               (setq acc (cons (list value key) acc)))
+             table)
+    acc))
+
+(defun fwf--sorted-buffer-words (from to)
+  (fwf--sorted-values
+   (fwf--count-words from to)))
+
+(defun fwf--print-results (table)
+  (mapc (lambda (tuple)
+          (message (format "%s %s" (car tuple) (cadr tuple))))
+        (fwf--sorted-values table)))
