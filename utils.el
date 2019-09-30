ce534880f483e620c607c478f94af2e9fa526dc7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-30 09:29:08 +0200

    Add method to measure the wall time
    Courtesy of stack overflow

---
+++ utils.el
@@ -0,0 +1,7 @@
+(provide 'utils)
+
+(defmacro measure-time (&rest body)
+  "Measure the time it takes to evaluate BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (message "%.06f" (float-time (time-since time)))))
